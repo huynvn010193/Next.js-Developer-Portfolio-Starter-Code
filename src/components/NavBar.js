4b245b76bdb932728666dbfa4c4938091d27fe84
huynvn <Huy.Nguyen@navigosgroup.com>
2023-10-26 00:05:05 +0700

    Done logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -29,31 +29,61 @@ const CustomLink = ({ href, title, className = "" }) => {
 
 const NavBar = () => {
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
       <nav>
-        <CustomLink href="/" title="Home" className="mr-4" />
-        <CustomLink href="/about" title="About" className="mx-4" />
-        <CustomLink href="/projects" title="Projects" className="mx-4" />
-        <CustomLink href="/articles" title="Articles" className="ml-4" />
+        <CustomLink href='/' title='Home' className='mr-4' />
+        <CustomLink href='/about' title='About' className='mx-4' />
+        <CustomLink href='/projects' title='Projects' className='mx-4' />
+        <CustomLink href='/articles' title='Articles' className='ml-4' />
       </nav>
-      <nav className="flex items-center justify-center flex-wrap">
-        <a href="https://twitter.com" target="_blank">
+      <nav className='flex items-center justify-center flex-wrap'>
+        <motion.a
+          href='https://twitter.com'
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className='w-6 mr-3'
+        >
           <TwitterIcon />
-        </a>
-        <a href="https://twitter.com" target="_blank">
+        </motion.a>
+        <motion.a
+          href='https://twitter.com'
+          target={"_blank"}
+          className='w-6 mr-3'
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
           <GithubIcon />
-        </a>
-        <a href="https://twitter.com" target="_blank">
+        </motion.a>
+        <motion.a
+          href='https://twitter.com'
+          target={"_blank"}
+          className='w-6 mr-3'
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
           <LinkedInIcon />
-        </a>
-        <a href="https://twitter.com" target="_blank">
+        </motion.a>
+        <motion.a
+          href='https://twitter.com'
+          target={"_blank"}
+          className='w-6 mr-3'
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
           <PinterestIcon />
-        </a>
-        <a href="https://twitter.com" target="_blank">
+        </motion.a>
+        <motion.a
+          href='https://twitter.com'
+          target={"_blank"}
+          className='w-6 mr-3'
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+        >
           <DribbbleIcon />
-        </a>
+        </motion.a>
       </nav>
-      <div className="absolute left-[50%] translate-x-[-50%]">
+      <div className='absolute left-[50%] translate-x-[-50%]'>
         <Logo />
       </div>
     </header>
