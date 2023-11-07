f8304302292c38cdc7ea74edc4a4740da3d1bbf0
huynvn010193 <hpn010193@gmail.com>
2023-11-07 22:46:46 +0700

    Pending dark mode

---
+++ src/components/hooks/useThemeSwither.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+
+const useThemeSwither = () => {
+  const preferDarkQuery = "(prefer-color-scheme: dark)";
+  const [mode, setMode] = useState("");
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(preferDarkQuery);
+    const usePref = window.localStorage.getItem("theme");
+
+    const handleChange = () => {
+      if (usePref) {
+        let check = usePref === "dark" ? "dark" : "light";
+        setMode(check);
+        if (check === "dark") {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      } else {
+        let check = mediaQuery.matches ? "dark" : "light";
+        setMode(check);
+        if (check === "dark") {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      }
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
+  useEffect(() =? {
+
+  }, [mode])
+
+  return <div>useThemeSwither</div>;
+};
+
+export default useThemeSwither;
