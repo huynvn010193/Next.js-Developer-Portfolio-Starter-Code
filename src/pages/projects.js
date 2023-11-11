070e33952a9c3f41abbed08aa24c03d4f8f5bf52
huynvn <Huy.Nguyen@navigosgroup.com>
2023-11-12 06:09:12 +0700

    Pending 5:26:48 project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -14,13 +14,14 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
     <article
       className='w-full flex items-center justify-between rounded-3xl border 
-      border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl dark:bg-dark dark:border-light'
+      border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl dark:bg-dark dark:border-light
+      lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4'
     >
       <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light' />
       <Link
         href={link}
         target='_blank'
-        className='w-1/2 cursor-pointer overflow-hidden rounded-lg'
+        className='w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full'
       >
         <FramerImage
           src={img}
@@ -32,14 +33,14 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
           sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'
         />
       </Link>
-      <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
-        <span className='text-primary font-medium text-xl dark:text-primaryDark'>
+      <div className='w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6'>
+        <span className='text-primary font-medium text-xl dark:text-primaryDark xs:text-base'>
           {type}
         </span>
         <Link
           href={link}
           target='_blank'
-          className='hover:underline underline-offset-2'
+          className='hover:underline underline-offset-2 '
         >
           <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light'>
             {title}
@@ -122,9 +123,9 @@ const projects = () => {
         <Layout className='pt-16'>
           <AnimatedText
             text='Imagination Trumps Knowledge!'
-            className='mb-16'
+            className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8'
           />
-          <div className='grid grid-cols-12 gap-24 gap-y-32'>
+          <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0'>
             <div className='col-span-12'>
               <FeaturedProject
                 title='Crypto Screener Application'
