47a457998763bd9a7cf540e082fb5486fca26ea3
huynvn010193 <hpn010193@gmail.com>
2023-11-11 21:59:33 +0700

    5:23:04: Done page About

--- src/components/Education.js
+++ src/components/Education.js
@@ -7,15 +7,18 @@ const Details = ({ type, time, place, info }) => {
   return (
     <li
       ref={ref}
-      className='my-8 first:mt-0 last:mt-0 w-[60%] mx-auto flex flex-col items-center justify-between'
+      className="my-8 first:mt-0 last:mt-0 w-[60%] mx-auto flex flex-col 
+      items-center justify-between md:w-[80%]"
     >
       <LiIcon reference={ref} />
       <motion.div initial={{ y: 50 }} whileInView={{ y: 0 }}>
-        <h3 className='capitalize font-bold text-2xl'>{type}&nbsp;</h3>
-        <span className='capitalize font-medium text-dark/75 dark:text-light/75'>
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+          {type}&nbsp;
+        </h3>
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
           {time} | {place}
         </span>
-        <p className='font-medium w-full'>{info}</p>
+        <p className="font-medium w-full md:text-sm">{info}</p>
       </motion.div>
     </li>
   );
@@ -29,34 +32,36 @@ const Education = () => {
   });
 
   return (
-    <div className='my-64'>
-      <h2 className='font-bold text-8xl mb-32 w-full text-center'>Education</h2>
-      <div className='w-[75%] mx-auto relative' ref={ref}>
+    <div className="my-64">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+        Education
+      </h2>
+      <div className="w-[75%] mx-auto relative lg:w-[90%] md:w-full" ref={ref}>
         <motion.div
-          className='absolute left-9 top-8 w-[4px] h-full bg-dark origin-top dark:bg-light'
+          className="absolute left-9 top-8 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]"
           style={{ scaleY: scrollYProgress }}
         />
-        <ul className='w-full flex flex-col items-start justify-between ml-4'>
+        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
           <Details
-            type='Bachelor Of Science In Computer Science'
-            time='2016-2020'
-            place='Stanford University'
-            info='Relevant courses included Data Structures and Algorithms, Computer Systems Engineering, and Artificial 
-            Inte'
+            type="Bachelor Of Science In Computer Science"
+            time="2016-2020"
+            place="Stanford University"
+            info="Relevant courses included Data Structures and Algorithms, Computer Systems Engineering, and Artificial 
+            Inte"
           />
           <Details
-            type='Master Of Computer Science'
-            time='2020-2022'
-            place='Stanford University'
+            type="Master Of Computer Science"
+            time="2020-2022"
+            place="Stanford University"
             info="Completed a master's project on deep learning, developing a new neural network architecture for natural 
             language understanding."
           />
           <Details
-            type='Online Coursework'
-            time='2016-2020'
-            place='Coursera And EdX'
-            info='Completed coursework in advanced topics such as Reinforcement Learning, Computer Vision, and Machine 
-            Learning Engineering.'
+            type="Online Coursework"
+            time="2016-2020"
+            place="Coursera And EdX"
+            info="Completed coursework in advanced topics such as Reinforcement Learning, Computer Vision, and Machine 
+            Learning Engineering."
           />
         </ul>
       </div>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,24 +7,24 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
   return (
     <li
       ref={ref}
-      className='my-8 first:mt-0 last:mt-0 w-[60%] mx-auto flex flex-col items-center justify-between'
+      className="my-8 first:mt-0 last:mt-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]"
     >
       <LiIcon reference={ref} />
       <motion.div initial={{ y: 50 }} whileInView={{ y: 0 }}>
-        <h3 className='capitalize font-bold text-2xl'>
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
           {position}&nbsp;{" "}
           <a
             href={companyLink}
-            target='_blank'
-            className='text-primary capitalize dark:text-primaryDark'
+            target="_blank"
+            className="text-primary capitalize dark:text-primaryDark"
           >
             @{company}
           </a>
         </h3>
-        <span className='capitalize font-medium text-dark/75 dark:text-light/75'>
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
           {time} | {address}
         </span>
-        <p className='font-medium w-full'>{work}</p>
+        <p className="font-medium w-full md:text-sm">{work}</p>
       </motion.div>
     </li>
   );
@@ -38,62 +38,62 @@ const Experience = () => {
   });
 
   return (
-    <div className='my-64'>
-      <h2 className='font-bold text-8xl mb-32 w-full text-center'>
+    <div className="my-64">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
         Experience
       </h2>
-      <div className='w-[75%] mx-auto relative' ref={ref}>
+      <div className="w-[75%] mx-auto relative lg:w-[90%] md:w-full" ref={ref}>
         <motion.div
-          className='absolute left-9 top-8 w-[4px] h-full bg-dark origin-top dark:bg-light'
+          className="absolute left-9 top-8 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]"
           style={{ scaleY: scrollYProgress }}
         />
-        <ul className='w-full flex flex-col items-start justify-between ml-4'>
+        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
           <Details
-            position='Software Engineer'
-            company='Google'
-            companyLink='http://www.google.com'
-            time='2022-Present'
-            address='Mountain View, CA'
+            position="Software Engineer"
+            company="Google"
+            companyLink="http://www.google.com"
+            time="2022-Present"
+            address="Mountain View, CA"
             work="Worked on a team responsible for developing new features for Google's 
               search engine, including improving the accuracy and relevance of search results and 
               developing new tools for data analysis and visualization."
           />
           <Details
-            position='Software Engineer'
-            company='Google'
-            companyLink='http://www.google.com'
-            time='2022-Present'
-            address='Mountain View, CA'
+            position="Software Engineer"
+            company="Google"
+            companyLink="http://www.google.com"
+            time="2022-Present"
+            address="Mountain View, CA"
             work="Worked on a team responsible for developing new features for Google's 
               search engine, including improving the accuracy and relevance of search results and 
               developing new tools for data analysis and visualization."
           />
           <Details
-            position='Software Engineer'
-            company='Google'
-            companyLink='http://www.google.com'
-            time='2022-Present'
-            address='Mountain View, CA'
+            position="Software Engineer"
+            company="Google"
+            companyLink="http://www.google.com"
+            time="2022-Present"
+            address="Mountain View, CA"
             work="Worked on a team responsible for developing new features for Google's 
               search engine, including improving the accuracy and relevance of search results and 
               developing new tools for data analysis and visualization."
           />
           <Details
-            position='Software Engineer'
-            company='Google'
-            companyLink='http://www.google.com'
-            time='2022-Present'
-            address='Mountain View, CA'
+            position="Software Engineer"
+            company="Google"
+            companyLink="http://www.google.com"
+            time="2022-Present"
+            address="Mountain View, CA"
             work="Worked on a team responsible for developing new features for Google's 
               search engine, including improving the accuracy and relevance of search results and 
               developing new tools for data analysis and visualization."
           />
           <Details
-            position='Software Engineer'
-            company='Google'
-            companyLink='http://www.google.com'
-            time='2022-Present'
-            address='Mountain View, CA'
+            position="Software Engineer"
+            company="Google"
+            companyLink="http://www.google.com"
+            time="2022-Present"
+            address="Mountain View, CA"
             work="Worked on a team responsible for developing new features for Google's 
               search engine, including improving the accuracy and relevance of search results and 
               developing new tools for data analysis and visualization."
