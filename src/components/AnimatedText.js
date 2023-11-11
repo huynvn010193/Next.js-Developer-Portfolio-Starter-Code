f97450b77ab731d576ca72aea9b15b6f304e70b5
huynvn <Huy.Nguyen@navigosgroup.com>
2023-11-11 10:44:23 +0700

    Pending reponsive

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -27,7 +27,7 @@ const singleQuote = {
 
 const AnimatedText = ({ text, className = "" }) => {
   return (
-    <div className='w-full mx-auto py-2 items-center justify-center text-center overflow-hidden'>
+    <div className='w-full mx-auto py-2 items-center justify-center text-center overflow-hidden sm:py-0'>
       <motion.h1
         className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className} dark:text-light`}
         variants={quote}
