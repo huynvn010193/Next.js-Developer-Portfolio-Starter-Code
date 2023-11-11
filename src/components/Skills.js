944c06b85978647f2d29c04fa03155e93177a418
huynvn010193 <hpn010193@gmail.com>
2023-11-11 21:36:40 +0700

    Done reponsive Skill

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,8 +4,10 @@ import { motion } from "framer-motion";
 const Skill = ({ name, x, y }) => {
   return (
     <motion.div
-      className='flex items-center justify-center rounded-full font-semibold bg-dark text-light 
-        p-8 shadow-dark cursor-pointer py-3 px-6 absolute dark:text-dark dark:bg-light'
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light 
+        p-8 shadow-dark cursor-pointer py-3 px-6 absolute dark:text-dark dark:bg-light
+        lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent
+        xs:text-dark xs:dark:text-light xs:font-bold"
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
       whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
@@ -20,23 +22,33 @@ const Skill = ({ name, x, y }) => {
 const Skills = () => {
   return (
     <>
-      <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skill</h2>
-      <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark'>
+      <h2 className="font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32">
+        Skill
+      </h2>
+      <div
+        className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark
+        lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] 
+        lg:bg-circularLightLg lg:dark:bg-circularDarkLg
+        md:bg-circularLightMd md:dark:bg-circularDarkMd
+        sm:bg-circularLightSm sm:dark:bg-circularDarkSm
+      "
+      >
         <motion.div
-          className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light'
+          className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer 
+          dark:text-dark dark:bg-light lg:p-6 md:p-4 xs:text-xs xs:p-2"
           whileHover={{ scale: 1.05 }}
         >
           Web
         </motion.div>
-        <Skill name='HTML' x='-25vw' y='2vw' />
-        <Skill name='CSS' x='-6vw' y='-10vw' />
-        <Skill name='Javascript' x='20vw' y='6vw' />
-        <Skill name='ReactJS' x='0vw' y='12vw' />
-        <Skill name='NextJS' x='-20vw' y='-15vw' />
-        <Skill name='NodeJS' x='15vw' y='-12vw' />
-        <Skill name='Figma' x='0vw' y='-20vw' />
-        <Skill name='Firebase' x='-25vw' y='18vw' />
-        <Skill name='Tailwind CSS' x='18vw' y='18vw' />
+        <Skill name="HTML" x="-25vw" y="2vw" />
+        <Skill name="CSS" x="-6vw" y="-10vw" />
+        <Skill name="Javascript" x="20vw" y="6vw" />
+        <Skill name="ReactJS" x="0vw" y="12vw" />
+        <Skill name="NextJS" x="-20vw" y="-15vw" />
+        <Skill name="NodeJS" x="15vw" y="-12vw" />
+        <Skill name="Figma" x="0vw" y="-20vw" />
+        <Skill name="Firebase" x="-25vw" y="18vw" />
+        <Skill name="Tailwind CSS" x="18vw" y="18vw" />
       </div>
     </>
   );
